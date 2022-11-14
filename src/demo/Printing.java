d3d82f24264529c89c5e944ef0b515ff001dd250
sharique393 <103323157+sharique393@users.noreply.github.com>
2022-11-14 10:56:06 +0530

    deleted

--- src/demo/Printing.java
+++ src/demo/Printing.java
@@ -7,7 +7,7 @@ public class Printing {
 	public static void main(String[] args) {
 		System.out.println("my name is sharique");
 		int sum= a+b;
-		System.out.println("sum of a number is:"  +sum);
+		System.out.println("sum of a number is addition:"  +sum);
 
 	}
 
